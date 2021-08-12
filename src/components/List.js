a8d3f66cbc6062da12795d7e965e8fea58acf7cf
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-08-11 20:29:56 -0400

    Added list filter

--- src/components/List.js
+++ src/components/List.js
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import Todo from "./Todo";
 
 const List = (props) => {
     const {list} = props;
+    const [filter, setFilter] = useState('All');
+
+    const filteredList = list.filter(item => item.status === filter)
 
     return (list) ? (
         <div id='todolist'>
-            {list.map((todo) => {
-                return <Todo todo={todo} key={Math.random()*100}/>
-            })}
+            <div>
+                <button onClick={()=> setFilter('All')}>All</button>
+                <button onClick={()=> setFilter('Completed')}>Completed</button>
+                <button onClick={()=> setFilter('Not Completed')}>Not Completed</button>
+            </div>
+            {(filter === 'All') ? (
+                list.map((todo) => {
+                    return <Todo todo={todo} key={Math.random()*100}/>
+                })
+                ) : (
+                    filteredList.map((todo) => {
+                        return <Todo todo={todo} key={Math.random()*100}/>
+                })
+            )}
         </div>
     ) : null;
 }
