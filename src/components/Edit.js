3a109700ef23ecd4752114b24376401622047c9b
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-08-16 20:33:17 -0400

    Added updateTodo functionality

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,44 +1,50 @@
 import React, { useState } from "react";
 import Task from "../methods/task";
 import saveTask from "../methods/saveTask";
+import { useDispatch } from "react-redux";
+import { updateTodo } from '../redux/todoSlice';
 
-const Input = (props) => {
+const Edit = (props) => {
     const {values} = props;
     const [title, setTitle] = useState(values.title);
     const [description, setDescription] = useState(values.description);
     const [date, setDate] = useState(values.date);
     const [priority, setPriority] = useState(values.priority);
     const [display, setDisplay] = useState(false);
+    const dispatch = useDispatch();
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const task = Task(title, values.id, description, date, priority);
         saveTask(task);
+        dispatch(updateTodo(task))
         setDisplay(false);
     }
 
     const editLabel = (display) ? 'Cancel' : 'Edit';
 
     return (display) ? (
-        <form onSubmit={(e) => handleSubmit(e)}>
-            <label htmlFor='title'>Title: </label>
-            <input type ='text' name='title' value={title} onChange={(e) =>setTitle(e.target.value)} required />
-            <label htmlFor='description'>Description: </label>
-            <textarea type ='text' name='description' value={description} onChange={(e) =>setDescription(e.target.value)} />
-            <label htmlFor='date'>Date: </label>
-            <input type ='date' name='date' value={date} onChange={(e) =>setDate(e.target.value)} required/>
-            <label htmlFor='priority'>Priority: </label>
-            <select value={priority} onChange={(e) =>setPriority(e.target.value)}>
-                <option value="Low">Low</option>
-                <option value="Medium">Medium</option>
-                <option value="High">High</option>
-            </select>
-            <button type='submit'>Ok</button>
-            <button onClick={() => setDisplay(!display)}>{editLabel}</button>
-        </form>
+        <div>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <label htmlFor='title'>Title: </label>
+                <input type ='text' name='title' value={title} onChange={(e) =>setTitle(e.target.value)} required />
+                <label htmlFor='description'>Description: </label>
+                <textarea type ='text' name='description' value={description} onChange={(e) =>setDescription(e.target.value)} />
+                <label htmlFor='date'>Date: </label>
+                <input type ='date' name='date' value={date} onChange={(e) =>setDate(e.target.value)} required/>
+                <label htmlFor='priority'>Priority: </label>
+                <select value={priority} onChange={(e) =>setPriority(e.target.value)}>
+                    <option value="Low">Low</option>
+                    <option value="Medium">Medium</option>
+                    <option value="High">High</option>
+                </select>
+                <button type='submit'>Ok</button>
+            </form>
+                <button onClick={() => setDisplay(!display)}>{editLabel}</button>
+        </div>
     ) : (
         <button onClick={() => setDisplay(!display)}>{editLabel}</button>
     );
 }
 
-export default Input;
+export default Edit;
--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -7,26 +7,38 @@ const todoSlice = createSlice({
     name: 'todo',
     initialState,
     reducers: {
-        addTodo (state, action) {
+        addTodo(state, action) {
             state.push(action.payload)
         },
-        changeStatus (state, action) {
+        changeStatus(state, action) {
             const {id, newStatus} = action.payload
             const itemToChange = state.find(item => item.id === id)
             if (itemToChange) {
                 itemToChange.status = newStatus
             }
         },
-        removeTodo (state, action) {
+        removeTodo(state, action) {
             const { id } = action.payload
             const itemToDelete = state.find(item => item.id === id)
             const indexToDelete = state.indexOf(itemToDelete)
-            state.splice(indexToDelete, 1)
+            if (indexToDelete) {
+                state.splice(indexToDelete, 1)
+            }
+        },
+        updateTodo(state, action) {
+            const { id, title, description, date, priority } = action.payload
+            const itemToUpdate = state.find(item => item.id === id)
+            if (itemToUpdate) {
+                itemToUpdate.title = title
+                itemToUpdate.description = description
+                itemToUpdate.date = date
+                itemToUpdate.priority = priority
+            }
 
         }
     }
 })
 
-export const { addTodo, changeStatus, removeTodo } = todoSlice.actions;
+export const { addTodo, changeStatus, removeTodo, updateTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
