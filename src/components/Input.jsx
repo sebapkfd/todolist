d90fb1241a8499d347d3a7591abbb55f3b5582ec
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-08-17 18:34:18 -0400

    Added max lengths

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -34,11 +34,30 @@ const Input = () => {
             <div>
                 <form onSubmit={(e) => handleSubmit(e)}>
                     <label htmlFor='title'>Title: </label>
-                    <input type ='text' name='title' value={title} onChange={(e) =>setTitle(e.target.value)} required />
+                    <input 
+                    type ='text' 
+                    name='title' 
+                    value={title} 
+                    required 
+                    maxLength={15}
+                    onChange={(e) =>setTitle(e.target.value)} 
+                    />
                     <label htmlFor='description'>Description: </label>
-                    <textarea type ='text' name='description' value={description} onChange={(e) =>setDescription(e.target.value)} />
+                    <textarea 
+                    type ='text' 
+                    name='description' 
+                    value={description} 
+                    maxLength={100}
+                    onChange={(e) =>setDescription(e.target.value)} 
+                    />
                     <label htmlFor='date'>Date: </label>
-                    <input type ='date' name='date' value={date} onChange={(e) =>setDate(e.target.value)} required/>
+                    <input 
+                    type ='date' 
+                    name='date' 
+                    value={date} 
+                    required
+                    onChange={(e) =>setDate(e.target.value)} 
+                    />
                     <label htmlFor='priority'>Priority: </label>
                     <select value={priority} onChange={(e) =>setPriority(e.target.value)}>
                         <option value="Low">Low</option>
